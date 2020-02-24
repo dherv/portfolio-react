2028c3697d2f8df45d264d76a02d77eec5d1f2fb
dherv <damien.herv@gmail.com>
2020-02-24 18:24:49 +0900

    feat: add pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Normalize } from "styled-normalize";
 import { GlobalStyle } from "./styled/GlobalStyle";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
 
 const App = () => {
   return (
@@ -11,14 +14,29 @@ const App = () => {
       <header>
         <h1>damien hervieux</h1>
         <ul>
-          <li>projects</li>
-          <li>works</li>
-          <li>skills</li>
-          <li>about me</li>
+          <li>
+            <Link to="/projects">projects</Link>
+          </li>
+          <li>
+            <Link to="/skills">skills</Link>
+          </li>
+          <li>
+            <Link to="/about">about me</Link>
+          </li>
         </ul>
       </header>
       <main>
-        <Switch></Switch>
+        <Switch>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/skills">
+            <Skills />
+          </Route>
+        </Switch>
       </main>
     </Router>
   );
---
+++ src/pages/Skills.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+const Skills = () => <div>skills</div>;
+export default Skills;
