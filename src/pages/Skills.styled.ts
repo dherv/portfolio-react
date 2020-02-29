9e80360074e2357d65f566f097aed378e3c7c11e
dherv <damien.herv@gmail.com>
2020-03-01 07:36:30 +0900

    feat(skills): add skills page

---
+++ src/pages/Skills.styled.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const SCSkills = {
+  Container: styled.div`
+    margin: 7rem 0 3rem 0;
+  `,
+  FlexContainer: styled.div`
+    display: flex;
+  `,
+  Paragraph: styled.div`
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 1rem;
+    width: 75%;
+    margin: 2rem 0;
+    font-size: 20px;
+    line-height: 1.5;
+  `,
+  InlineTitle: styled.div`
+    font-weight: 800;
+    margin-right: 1rem;
+    color: #484848;
+  `,
+  InlineText: styled.div`
+    margin-right: 1rem;
+  `
+};
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,3 +1,40 @@
 import React from "react";
-const Skills = () => <div>skills</div>;
+import { SCG } from "../styled/GlobalComponents";
+import { SCSkills as SC } from "./Skills.styled";
+const Skills = () => (
+  <SC.Container>
+    <SCG.Title>Skills</SCG.Title>
+    <SC.Paragraph>
+      <SC.InlineTitle>frontend:</SC.InlineTitle>
+      <SC.InlineText>
+        React, Javascript, Typescript, NextJS, GraphQL, Websockets, GatsbyJS,
+        VueJS, HTML, CSS
+      </SC.InlineText>
+
+      <SC.InlineTitle>backend:</SC.InlineTitle>
+      <SC.InlineText>
+        NodeJS, NestJS, Express, Laravel, Django, MySQL, MongoDB, ElasticSearch,
+        GraphQL, Websockets
+      </SC.InlineText>
+
+      <SC.InlineTitle>devops:</SC.InlineTitle>
+      <SC.InlineText>
+        Docker, CircleCI, AWS S3, AWS EC2, AWS RDS, AWS Polly, Google Cloud
+        Translate and Google Cloud Text to speech
+      </SC.InlineText>
+
+      <SC.InlineTitle>test:</SC.InlineTitle>
+      <SC.InlineText>Jest, React Testing Library</SC.InlineText>
+
+      <SC.InlineTitle>tools:</SC.InlineTitle>
+      <SC.InlineText>
+        Webpack, Visual Studio Code, JetBrains, ESLint/TSLint, Prettier, Figma,
+        Trello, Jira, Github, GitLab, SourceTree, Postman, Slack
+      </SC.InlineText>
+
+      <SC.InlineTitle>mobile:</SC.InlineTitle>
+      <SC.InlineText>Swift</SC.InlineText>
+    </SC.Paragraph>
+  </SC.Container>
+);
 export default Skills;
