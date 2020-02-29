c66156b4ee3eeec3e3b25d09f5af0398b5244942
dherv <damien.herv@gmail.com>
2020-03-01 08:24:26 +0900

    feat(about): add about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,17 @@
 import React from "react";
-const About = () => <div>about me</div>;
+import { SCG } from "../styled/GlobalComponents";
+const About = () => (
+  <SCG.Container>
+    <SCG.Title>About me</SCG.Title>
+    <SCG.Paragraph>
+      Hello, my name is Damien. I'm 34 years old. My family and I live in
+      Tokyo.  I also used to live in Australia and in France where I am from. I
+      have been a Fullstack engineer in a Japanese startup for two years now. 
+      In my free time, I enjoy coding and learning new technologies. I also like
+      spending time at the jiujitsu club and playing with my kids. In a near
+      future I would like learn surfing and start working on an open source
+      project.
+    </SCG.Paragraph>
+  </SCG.Container>
+);
 export default About;
--- src/styled/GlobalComponents.tsx
+++ src/styled/GlobalComponents.tsx
@@ -1,9 +1,18 @@
 import styled from "styled-components";
 
 export const SCG = {
+  Container: styled.div`
+    margin: 7rem 0 3rem 0;
+  `,
   Title: styled.h1`
     font-weight: 800;
     font-size: 2.5rem;
+  `,
+  Paragraph: styled.p`
+    width: 60%;
+    margin: 2rem 0;
+    font-size: 20px;
+    line-height: 1.5;
   `,
   ButtonVisit: styled.button`
     margin-left: auto;
